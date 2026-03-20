2cd14d49374c02585f3653ddf69f0ba55d400651
MichalGrzech <michalgrzech93@gmail.com>
2026-03-20 22:55:40 +0100

    fix: fix orderService variable name

--- src/RecruProject.Console/Program.cs
+++ src/RecruProject.Console/Program.cs
@@ -11,19 +11,19 @@ services.Register();
 // Build
 var servicesProvider = services.BuildServiceProvider();
 var logger = servicesProvider.GetRequiredService<ILogger>();
-var orderService1 = servicesProvider.GetRequiredService<IOrderService>();
+var orderService = servicesProvider.GetRequiredService<IOrderService>();
 
 // Resolve
 // Adding task using 'await' keyword - first approach
-await orderService1.AddOrderAsync(new Order { Id = 1, Description = "Laptop" });
-await orderService1.AddOrderAsync(new Order { Id = 2, Description = "Phone" });
-await orderService1.AddOrderAsync(new Order { Id = 2, Description = "Computer" }); // Force error
+await orderService.AddOrderAsync(new Order { Id = 1, Description = "Laptop" });
+await orderService.AddOrderAsync(new Order { Id = 2, Description = "Phone" });
+await orderService.AddOrderAsync(new Order { Id = 2, Description = "Computer" }); // Force error
 
 // Processing task using Task.WaitAll - second approach
 var tasks = new Task[3];
-tasks[0] = orderService1.ProcessOrderAsync(1);
-tasks[1] = orderService1.ProcessOrderAsync(2);
-tasks[2] = orderService1.ProcessOrderAsync(-1);
+tasks[0] = orderService.ProcessOrderAsync(1);
+tasks[1] = orderService.ProcessOrderAsync(2);
+tasks[2] = orderService.ProcessOrderAsync(-1);
 Task.WaitAll(tasks);
 
 logger.LogInfo("All orders processed.");
