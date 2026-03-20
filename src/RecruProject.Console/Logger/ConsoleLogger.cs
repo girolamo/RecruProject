c128e664543419b8d7a851911ae64b4edfafee6a
MichalGrzech <michalgrzech93@gmail.com>
2026-03-20 22:06:35 +0100

    fix: fix logs whitespaces

--- src/RecruProject.Console/Logger/ConsoleLogger.cs
+++ src/RecruProject.Console/Logger/ConsoleLogger.cs
@@ -12,11 +12,11 @@ public class ConsoleLogger(IConfiguration configuration) : ILogger
         if (string.Equals(logLevel, "Error", StringComparison.OrdinalIgnoreCase))
             return;
         
-        System.Console.WriteLine($"[{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}][INFO]{message}");
+        System.Console.WriteLine($"[{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}] [INFO] {message}");
     }
 
     public void LogError(string message, Exception ex)
     {
-        System.Console.WriteLine($"[{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}][ERROR]{message}.\nException message: {ex.Message}\nStackTrace: {ex.StackTrace}");
+        System.Console.WriteLine($"[{DateTime.UtcNow:yyyy-MM-dd HH:mm:ss}] [ERROR] {message}.\nException message: {ex.Message}\nStackTrace: {ex.StackTrace}");
     }
 }
